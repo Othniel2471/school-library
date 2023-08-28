1db32e96173dcecff05573c8ccf7f8e913fe2a22
othniel2471 <othnielomaidu@gmail.com>
2023-08-28 17:23:18 +0100

    created classes for person,student and teacher

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(age, name: "Unknown", parents_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parents_permission = parents_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  private def of_age
+    @age >= 18
+    return true
+  end
+
+  def can_use_services?
+    @parent_permission || of_age?
+  end
+end
