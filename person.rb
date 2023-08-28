0b1534099aeceea0f0d88b73b9a4c20d871f954d
othniel2471 <othnielomaidu@gmail.com>
2023-08-28 20:41:35 +0100

    fixed public method in Person class

--- person.rb
+++ person.rb
@@ -9,13 +9,14 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
+
+  def can_use_services?
+    @parent_permission || of_age?
+  end
+
   private
 
   def of_age
     @age >= 18
-  end
-
-  def can_use_services?
-    @parent_permission || of_age?
   end
 end
