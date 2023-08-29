8d338da00e883301d4fe1056393ec2704f997f3c
othniel2471 <othnielomaidu@gmail.com>
2023-08-29 15:00:03 +0100

    added decorators

--- person.rb
+++ person.rb
@@ -1,5 +1,10 @@
-class Person
-  def initialize(age, name: 'Unknown', parents_permission: true)
+require './nameable'
+require './decorator'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parents_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -13,9 +18,20 @@ class Person
     @parent_permission || of_age?
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
