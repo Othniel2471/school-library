bf0e933d0d0285c355f238debc289193096fdbb9
othniel2471 <othnielomaidu@gmail.com>
2023-08-30 14:25:02 +0100

    modified student.rb

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './Person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, name: 'Unknown', parents_permission: true)
     super(age, name: name, parents_permission: parents_permission)
