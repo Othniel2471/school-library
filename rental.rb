ab02fc1ef56fccfbc270013ce0a8958b42905fac
othniel2471 <othnielomaidu@gmail.com>
2023-08-30 15:53:43 +0100

    updated rental.rb

--- rental.rb
+++ rental.rb
@@ -1,5 +1,5 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
@@ -8,15 +8,5 @@ class Rental
 
     book.rentals.push(self)
     person.rentals.push(self)
-  end
-
-  def book(book)
-    @book = book
-    Book.add_rental(self)
-  end
-
-  def person(person)
-    @person = person
-    Person.add_rental(self)
   end
 end
