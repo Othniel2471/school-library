b3a171f84ea187f1cd538df6521f7f46c62fa7c1
othniel2471 <othnielomaidu@gmail.com>
2023-08-30 14:36:09 +0100

    fixed linters

--- rental.rb
+++ rental.rb
@@ -1,13 +1,14 @@
 class Rental
-  attr_accessor :date,
-                def initialize(date, book, person)
-                  @date = date
-                  @book = book
-                  @person = person
+  attr_accessor :date
 
-                  book.rentals.push(self)
-                  person.rentals.push(self)
-                end
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+
+    book.rentals.push(self)
+    person.rentals.push(self)
+  end
 
   def book(book)
     @book = book
