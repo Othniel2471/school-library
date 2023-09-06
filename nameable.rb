51f2a45dfa5a0b051fa43298f0e5bbe78d7bd3aa
othniel2471 <othnielomaidu@gmail.com>
2023-09-06 12:02:45 +0100

    fixed linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Nameable
   def correct_name
     raise NotImplementedError, 'You must implement the correct_name method'
--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './nameable'
 require './decorator'
 require './capitalize_decorator'
